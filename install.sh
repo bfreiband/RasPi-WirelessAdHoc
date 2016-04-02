278d49c674b7114ca4825eeb8efdf78229d021de
Ben Freiband <bfreiband@gmail.com>
2016-04-02 05:56:01 -0400

    Added isc-dhcp-server, and did install.sh stuff

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-#the stuff goes here
+echo "Copying files"
+cp /full_conf_files/dhcpd.conf /etc/dhcp/
+cp /full_conf_files/rc.local /etc/
+cp /full_conf_files/interfaces /etc/network/
+cp /full_conf_files/isc-dhcp-server /etc/default/
+echo "Process complete"
