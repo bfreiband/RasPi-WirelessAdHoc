23c1d020bf806438afd54a35568cf78a98f65a90
Matheus Faria de Alencar <malencar@pm.me>
2020-08-29 17:36:50 -0300

    fix: incorrect script path

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Copying files"
-cp /full_conf_files/dhcpd.conf /etc/dhcp/
-cp /full_conf_files/rc.local /etc/
-cp /full_conf_files/interfaces /etc/network/
-cp /full_conf_files/isc-dhcp-server /etc/default/
+cp full_conf_files/dhcpd.conf /etc/dhcp/
+cp full_conf_files/rc.local /etc/
+cp full_conf_files/interfaces /etc/network/
+cp full_conf_files/isc-dhcp-server /etc/default/
 echo "Process complete"
